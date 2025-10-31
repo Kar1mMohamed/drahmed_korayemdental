f69ed24826819fdb59f562c8a8e8f694b82ed8f6
Kar1mMohamed <karimo741852@gmail.com>
2025-10-31 09:30:03 +0200

    ..

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        echo "RQWE";
+        
         // Share transformations data with welcome view
         view()->composer('welcome', function ($view) {
             $view->with('transformations', config('transformations.items'));
